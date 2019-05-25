767810a609526e4145dc58194ad2507c896d3b5d
Keven Leone <keven.dos.s.leone@accenture.com>
2019-05-24 23:57:14 -0300

    New amqp-stats version, now with Promises and Detailed API Info using Jsdoc, and now supporting new Engines Version, since version 8.

--- tests/test.js
+++ tests/test.js
@@ -1,20 +1,27 @@
-var Stats = require('../index.js');
-var conf = require('./conf.json');
 
-var stats = new Stats(conf); // defaults to localhost, be sure you have mgmt plugin installed: http://www.rabbitmq.com/management.html
+const AmqpStats = require('../index');
 
-stats.whoami(function (err, res, data) {
-  if(err) { throw err; }
-  console.log('data: ', data);
-});
-
-stats.queues(function(err, res, data){
-  if (err) { throw err; }
-  console.log('data: ', data);
-  data.forEach(function (queue) {
-    if(queue.message_stats){
-      console.log('name: ', queue.name);
-      console.log('message_stats: ', queue.message_stats.deliver_get);
+class Test {
+    constructor() {
+        this.Rabbit = new AmqpStats({
+            username: 'username',
+            password: 'password',
+            hostname: 'hostname/rabbit',
+            protocol: 'https'
+        });
     }
-  });
-});
+
+    async overview() {
+        const overview = await this.Rabbit.overview()
+        console.log(overview)
+    }
+
+    async connections() {
+        const connections = await this.Rabbit.connections();
+        console.log(connections);
+    }
+}
+
+const TestV1 = new Test();
+TestV1.connections();
+TestV1.overview();
